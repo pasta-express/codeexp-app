4c99b257382bf3e6a2e675ac60b4cafb69552b22
fairyinabottle4 <keithlow98@gmail.com>
2021-06-15 21:06:06 +0800

    Fix bugs

--- profileScreen/ProfileStackScreen.js
+++ profileScreen/ProfileStackScreen.js
@@ -1,3 +1,6 @@
+import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+
 export function ProfileStackScreen() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
