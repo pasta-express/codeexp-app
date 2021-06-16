664f0d0049a7e7bc5148b3cd153a335313b64edc
Icelenaugust <dingyoujia0823@gmail.com>
2021-06-16 22:14:39 +0800

    minor mistake

--- InboxPage/Conversation.js
+++ InboxPage/Conversation.js
@@ -1,53 +1,14 @@
 import * as React from "react";
 
-import { View, Text, StyleSheet, FlatList, TextInput, SafeAreaView} from "react-native";
+import { View, Text, StyleSheet, FlatList, SafeAreaView} from "react-native";
 
-import { Card, List, Colors} from "react-native-paper";
+import { TextInput, List, Colors} from "react-native-paper";
 
-const profile = {"Merlin": Colors.grey500, "Jerry": Colors.red500}
-
-// const SAMPLE_CONVERSATION=[{
-// id:"0",
-// user:"Merlin",
-// data: "Hello"
-// }]
-
-export default function ConversationScreen({navigation, route}) {
-  const [conversation, setConversation] = React.useState([
-  ])
-  const [text, setText] = React.useState("");
-
-  function SendMessage(){
-    setConversation([
-      ...conversation, 
-      {user:"Merlin", data:text, id:`${conversation.length}`},
-      {user: route.params?.contact, data:text, id:`${conversation.length + 1}`}
-    ])
-    setText('');
-  }
-
-  return (
-    <SafeAreaView style={styles.Container}>
-      <SafeAreaView style={{flex: 10}}>
-        <FlatList style={styles.list} data={conversation} renderItem={renderMsg}/>
-      </SafeAreaView>
-      <SafeAreaView style={{flex: 1}}> 
-        <TextInput style={styles.InputBar}
-          placeholder="Send a message"
-          value={text}
-          onChangeText={setText}
-          onSubmitEditing = {SendMessage}
-          />
-      </SafeAreaView>
-    </SafeAreaView>
-  );
-
-}
+const profile = {"You": Colors.grey500, "Jerry": Colors.blue500}
 
 function renderMsg({item}){
   return <MsgBubble user={item.user} text={item.data}/>
 }
-
 
 const MsgBubble = (props) => {
   const color = profile[props.user]
@@ -60,22 +21,66 @@ const MsgBubble = (props) => {
   )
 }
 
+//Must pass params:{contact: "user"}
+export default function ConversationScreen({navigation, route}) {
+  const contact = route.params?.contact ? route.params?.contact: "Jerry";
+  const SAMPLE_CONVERSATION=[
+    {id:"0", user:"You", data:"Hello!"},
+    {id:"1", user:contact, data:"Hi! do you have any queries?"},
+    ]
+
+  const [conversation, setConversation] = React.useState(SAMPLE_CONVERSATION)
+  const [text, setText] = React.useState("");
+
+  function SendMessage(){
+    setConversation([
+      ...conversation, 
+      {user:"You", data:text, id:`${conversation.length}`},
+    ])
+    setText('');
+  }
+
+  function InputBar(){
+    return( <TextInput style={styles.InputBar}
+      placeholder="Send a message"
+      value={text}
+      onChangeText={setText}
+      right={<TextInput.Icon name="send" onPress={SendMessage}/>}
+      />
+    );
+  }
+
+  return (
+    <SafeAreaView style={styles.Container}>
+      <SafeAreaView style={styles.ListContainer}>
+        <FlatList style={styles.list} data={conversation} renderItem={renderMsg}/>
+      </SafeAreaView>
+      <SafeAreaView style={styles.InputContainer}> 
+        {InputBar()}
+      </SafeAreaView>
+    </SafeAreaView>
+  );
+
+}
+
 const styles = StyleSheet.create({
-  InputBar: {
-      height: 40,
-      margin: 12,
-      borderWidth: 1,
-  },
   Container:{
     backgroundColor: "white",
-    flexDirection: "column"
+    flex: 1
   },
-
-  list:{
-  },
-
+  
   listContainer:{
     flex: 10
+  },
+
+  InputContainer: {
+      height: 40,
+      margin: 12,
+      flex: 1,
+  },
+
+  List:{
+    
   }
 
 });
