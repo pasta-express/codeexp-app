d8e6fc22c6978c12a68edd2128b59a816e92d3c1
fairyinabottle4 <keithlow98@gmail.com>
2021-06-16 18:48:54 +0800

    amendments to profile page

--- profileScreen/CurrentBookingsComponent.js
+++ profileScreen/CurrentBookingsComponent.js
@@ -1,20 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, FlatList } from "react-native";
 import { CurrentBookingCard } from "./CurrentBookingCard";
+import firebase from "firebase";
+import { firebaseConfig } from "../config/firebaseConfig";
+//firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}else {
+  firebase.app(); // if already initialized, use that one
+}
+
+const db = firebase.firestore().collection("sample-listings")
 
 export const CurrentBookingsComponent = (props) => {
   const { bookings } = props;
+  const currentBookingIds = bookings[0].currentBookings
+  const [listings, setListings] = useState([])
+
+  useEffect(() => {
+    const unsubscribe = db.onSnapshot((collection) => {
+      const listings = collection.docs.map((doc) => {
+        return {
+          id: doc.id,
+          ...doc.data()
+        };
+      });
+      setListings(listings);
+    });
+    return (() => {
+      unsubscribe();
+    });
+  }, []);
+
+  // listings.filter(listing => {
+  //   currentBookingIds.forEach(id => {
+  //     console.log("hello")
+  //     listing.id !== id
+  //   })
+  // })
 
   function renderItem({ item }) {
     const { id } = item;
-
     return <CurrentBookingCard key={id} bookingDetails={item} />;
   }
 
   return (
     <View>
       <Text>Current Bookings</Text>
-      <FlatList data={bookings} renderItem={renderItem} />
+      <FlatList data={listings} renderItem={renderItem} />
     </View>
   );
 };
--- profileScreen/ProfileScreen.js
+++ profileScreen/ProfileScreen.js
@@ -1,60 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SafeAreaView, StyleSheet, View } from "react-native";
 import { SignOutButton } from "./SignOutButton";
 import { ProfileHeaderCard } from "./ProfileHeaderCard";
 import { CurrentBookingsComponent } from "./CurrentBookingsComponent";
 import { ShadowEffectCard } from "./ShadowEffectCard";
+import firebase from "firebase";
+import { firebaseConfig } from "../config/firebaseConfig";
+//firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}else {
+  firebase.app(); // if already initialized, use that one
+}
 
+const db = firebase.firestore().collection("sample-users")
+
+console.log(db)
 const SAMPLE_IMAGE_URL =
   "https://locations-api-production.imgix.net/locations/image/35be52d4-1240-11eb-af66-0eb0aa9dee1d/Web_150DPI-20200908_WeWork_9_Battery_Rd_-_Singapore_005.jpg?auto=format%20compress&fit=crop&q=50&w=1800&h=1013";
-
-const SAMPLE_LISTINGS = [
-  {
-    id: "0",
-    companyName: "Company1",
-    coverImage: SAMPLE_IMAGE_URL,
-    price: 30,
-    location: "Singapore",
-    startDate: Date.now(),
-    endDate: Date.now(),
-  },
-  {
-    id: "1",
-    companyName: "Company2",
-    coverImage: SAMPLE_IMAGE_URL,
-    price: 40,
-    location: "Singapore",
-    startDate: Date.now(),
-    endDate: Date.now(),
-  },
-  {
-    id: "2",
-    companyName: "Company3",
-    coverImage: SAMPLE_IMAGE_URL,
-    price: 20,
-    location: "Singapore",
-    startDate: Date.now(),
-    endDate: Date.now(),
-  },
-  {
-    id: "3",
-    companyName: "Company4",
-    coverImage: SAMPLE_IMAGE_URL,
-    price: 20,
-    location: "Singapore",
-    startDate: Date.now(),
-    endDate: Date.now(),
-  },
-  {
-    id: "4",
-    companyName: "Company5",
-    coverImage: SAMPLE_IMAGE_URL,
-    price: 10,
-    location: "Singapore",
-    startDate: Date.now(),
-    endDate: Date.now(),
-  },
-];
 
 const SAMPLE_USER = {
   name: "Tom",
@@ -76,30 +39,33 @@ const SAMPLE_USER = {
   ],
 };
 const ProfileScreen = (props) => {
-  // supposed to receive User object and Sample Listings from DB
+  const [bookedListings, setBookedListings] = useState([])
+  console.log("why")
+  useEffect(() => {
+    console.log("hello world")
+    //this could be wonky
+    const unsubscribe = db.onSnapshot((collection) => {
+      const bookedListings = collection.docs.map((doc) => {
+        return {
+          id: doc.id,
+          currentBookings: doc.data().currentBookings
+        };
+      });
+      setBookedListings(bookedListings)
+    });
+    return (() => {
+      unsubscribe;
+    });
+  }, [])
 
-  const getCurrentBookingsDetails = () => {
-    const currentBookings = SAMPLE_USER.currentBookings;
-    const currentBookingsIDs = [];
-    for (let i = 0; i < currentBookings.length; i++) {
-      const bookingID = currentBookings[i].id;
-      currentBookingsIDs.push(bookingID);
-    }
-
-    // iterate thru all listings to get listing details
-    const bookingDetails = SAMPLE_LISTINGS.filter((listing) =>
-      currentBookingsIDs.includes(listing.id)
-    );
-    return bookingDetails;
-  };
-
-  const currentBookingsDetails = getCurrentBookingsDetails();
+  console.log('hello')
+  console.log(bookedListings)
 
   return (
     <SafeAreaView style={styles.container}>
       <ProfileHeaderCard user={SAMPLE_USER} />
       <ShadowEffectCard>
-        <CurrentBookingsComponent bookings={currentBookingsDetails} />
+        <CurrentBookingsComponent bookings={bookedListings} />
       </ShadowEffectCard>
       <ShadowEffectCard>
         <SignOutButton />
