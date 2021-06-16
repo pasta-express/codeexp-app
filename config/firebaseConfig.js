4f54f68d750c97ee1dfa02c9d0f718bf7e7579e9
Icelenaugust <dingyoujia0823@gmail.com>
2021-06-16 19:50:25 +0800

    set user db

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -4,5 +4,6 @@ export const firebaseConfig = {
     projectId: "we-share-efc5d",
     storageBucket: "we-share-efc5d.appspot.com",
     messagingSenderId: "730990039197",
-    appId: "1:730990039197:web:5db00c5422efdccafdc474"
+    appId: "1:730990039197:web:5db00c5422efdccafdc474",
+    databaseURL: "https://we-share-efc5d-default-rtdb.asia-southeast1.firebasedatabase.app/",
 };
