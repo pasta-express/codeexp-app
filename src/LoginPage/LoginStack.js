c70381c1d7a27a721206062fbb4efb46e658d357
fairyinabottle4 <keithlow98@gmail.com>
2021-07-29 13:28:33 +0800

    Refactor into src

--- src/LoginPage/LoginStack.js
+++ src/LoginPage/LoginStack.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import LoginScreen from './Login'
-import Signup from "./Signup";
+import Signup from "../Signup";
 
 const InnerStack = createStackNavigator()
 
